8d95bdc915ea572d45fbc77a8bb4dc1a71b48d8e
Akhil Goyal <akhilg642@gmail.com>
2022-09-04 11:21:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program.connection();
+            Console.ReadLine();
+        }
+
+        public static void connection()
+        {
+            String cs = "Data Source=localhost;Initial catalog=PAYROLL_SERVICE;Trusted_Connection=True"; 
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    if (con.State == System.Data.ConnectionState.Open)
+                        Console.WriteLine("Connection successful");
+
+                    string query = "select * from EMPLOYEE_PAYROLL";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while(dr.Read())
+                    {
+                        Console.WriteLine("ID = " + dr["ID"] + " Name = " + dr["Name"] + " Salary = " + dr["Salary"]);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        
+        }
+    }
+}
